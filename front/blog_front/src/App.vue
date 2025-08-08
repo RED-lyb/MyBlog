55726aa6fa8ecfec265221529e1d65d233d3d773
李远博 <15566061+REDlyb@user.noreply.gitee.com>
2025-08-08 09:51:31 +0800

    项目初始化

---
+++ front/blog_front/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import home from './pages/home.vue'
+import router from './router'
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style scoped></style>
---
+++ front/blog_front/src/pages/home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+import {useStore} from '../stores/counter.js'
+import axios from 'axios'
+const stores = useStore()
+const data = ref("")
+const status = ref("")
+axios.get("http://localhost:8000/").then(res => { 
+    data.value = res.data
+    status.value = res.status
+})
+
+</script>
+<template>
+    <h1>目前正处于测试部署状态，还未正式部署相关内容</h1>
+    <p>测试后端返回信息：data:{{data}},status:{{status}}</p>
+</template>
