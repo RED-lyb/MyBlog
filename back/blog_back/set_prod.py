d89d52e48cb342a27bc4c66f589df1bfd48a6230
李远博 <15566061+REDlyb@user.noreply.gitee.com>
2025-08-19 07:35:03 +0800

    修改部分配置

--- back/blog_back/set_prod.py
+++ back/blog_back/set_prod.py
@@ -3,7 +3,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'webproject',
-        'USER': 'root',
+        'USER': 'admin',
         'PASSWORD': 'xxx',#生产环境建议修改密码
         'HOST': 'xxx',#改为对应数据库服务器的IP
     }
