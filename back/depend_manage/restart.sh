8535bb510877b2dc5824d1edcfffeaad2005f6ce
REDlyb <1874853966@qq.com>
2026-09-09 09:54:43 +0800

    优化重启脚本

--- back/depend_manage/restart.sh
+++ back/depend_manage/restart.sh
@@ -1,2 +1,5 @@
-uwsgi --stop /webproject/my-blog/project-master.pid 
+uwsgi --stop /webproject/my-blog/project-master.pid
+echo "已关闭uwsgi，重启中"
+sleep 2
 uwsgi --ini /webproject/my-blog/back/depend_manage/uwsgi.ini 
+echo "重启完成"
